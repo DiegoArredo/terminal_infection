d9aa3ab5aeae3bc314819500faa10bc669a1e55d
DiegoArredo <diego.arredo18@gmail.com>
2024-05-08 23:28:29 -0400

    Se mejora el input del movimiento del personaje

--- Player/player_1.gd
+++ Player/player_1.gd
@@ -6,20 +6,11 @@ var speed = 100
 @onready var animation = get_node("AnimationPlayer")
 
 func _physics_process(delta):
-
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("right"):
-		velocity.x += 1
-	if Input.is_action_pressed("left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("down"):
-		velocity.y += 1
-	if Input.is_action_pressed("up"):
-		velocity.y -= 1
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-
+	
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	move_and_slide()
+	
 	if velocity.x == 0 and velocity.y == 0:
 		animation.play("idle")
 	if velocity.x == 0 and velocity.y < 0:
@@ -33,7 +24,6 @@ func _physics_process(delta):
 		$AnimatedSprite2D.flip_h = velocity.x > 0
 	
 	
-	position += velocity * delta
 	if Input.is_action_just_pressed("disparo"):
 		disparar()
 
