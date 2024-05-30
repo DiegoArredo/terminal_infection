853f788c58a6b27909069bbe4c18b2184e65031f
brayanpizarro <brayan.pizarro@alumnos.ucn.cl>
2024-05-29 22:08:33 -0400

    "modificacion layer and mask player"

--- Enemies/enemy_1.gd
+++ Enemies/enemy_1.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
-@onready var _animacion_sprite =$AnimatedSprite2D
+
+
 @onready var player= get_node("/root/world_1/Player/Player_1")
 var heal=6
 func  _physics_process(delta):
 	var direction=global_position.direction_to(player.global_position)
 	velocity =direction* 50
 	move_and_slide()
+	
 func take_damage():
 	heal-=1
 	if heal==0:
