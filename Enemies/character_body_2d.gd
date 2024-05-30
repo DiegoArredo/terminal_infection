6a2c8b571f0315a0b8375d28c62e345c6746c65c
brayanpizarro <brayan.pizarro@alumnos.ucn.cl>
2024-05-29 21:24:58 -0400

    animacion de muerte enemigos

--- Enemies/character_body_2d.gd
+++ Enemies/character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
-
+@onready var _animated_sprite=$AnimatedSprite2D
 @onready var player= get_node("/root/world_1/Player/Player_1")
-var heal=3
+var heal=2
 func  _physics_process(delta):
 	var direction=global_position.direction_to(player.global_position)
 	velocity =direction* 50
@@ -9,5 +9,5 @@ func  _physics_process(delta):
 func take_damage():
 	heal-=1
 	if heal==0:
+		_animated_sprite.play("hurt")
 		queue_free()
-
