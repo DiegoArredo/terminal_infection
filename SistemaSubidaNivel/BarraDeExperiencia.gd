5796fc83b95f316b200a18d290213a183566eae8
ignacio <ihelocarraco@gmail.com>
2024-06-04 16:53:13 -0400

    aliniada ventana

--- SistemaSubidaNivel/BarraDeExperiencia.gd
+++ SistemaSubidaNivel/BarraDeExperiencia.gd
@@ -1,6 +1,8 @@
 extends ProgressBar
 
+signal level_up
 
+var valor_orbe = 50
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,5 +13,15 @@ func _process(delta):
 	pass
 
 
-func _on_orbe_experiencia_area_entered(area):
-	pass # Replace with function body.
+
+
+func _on_value_changed(value):
+	if (value == 100):
+		level_up.emit()
+	
+
+
+func _on_orbe_experiencia_body_entered(body):
+	if (body.is_in_group("player")):
+		value = value + valor_orbe
+		
---
+++ guiLevelUp.gd
@@ -0,0 +1,67 @@
+extends Control
+signal pedir_datos_a_player(localData)
+var datos_from_player
+var number_of_labels_in_container = 5
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	test()
+	
+func test():
+	visible = true
+	mock_datos_conseguidos_de_player()
+	insertar_datos()
+
+
+func _from_level_up():
+	get_parent().visible = true
+	mock_datos_conseguidos_de_player()
+	insertar_datos()
+	
+	
+	
+func datos_conseguidos_de_player():
+	await pedir_datos_a_player.emit(datos_from_player)	
+
+	
+func mock_datos_conseguidos_de_player():
+	datos_from_player = {
+	"punteria": 10,
+	"destreza": 20,
+	"fuerza": 30,
+	"inteligencia": 40,
+	"reflejos": 50
+}
+
+	
+func insertar_datos():
+	var gridContainer = getGridContainer()
+	var labels = extraerLabels(gridContainer)
+	ingresar_en_labels(labels)
+	
+func getGridContainer():
+	return $PanelContainer3/GridContainer
+		
+func extraerLabels(gridContainer):
+	var labels = []
+	for column in range(number_of_labels_in_container):
+		var label = getLabelFromColumn(column, gridContainer)
+		labels.append(label)
+	return labels
+		
+func getLabelFromColumn(column, gridContainer):
+		if column == 0:
+			return gridContainer.get_node("Label")
+		else:
+			return gridContainer.get_node("Label" + str(column + 1))
+	
+	
+		
+func ingresar_en_labels(labels):
+	for i in range(labels.size()):
+		var label = labels[i]
+		var propiedad = datos_from_player.keys()[i]
+		var valor = datos_from_player.values()[i]
+		label.setText(propiedad, valor)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
