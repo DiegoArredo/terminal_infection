3666d77f0c68cf9a24c5ecf013d6c68924e6c57d
Diego Arredondo <diego.arredo18@gmail.com>
2024-06-07 15:08:34 -0400

    Se mejora el spawn de enemigos

--- SpawnEnemy/Area2D.gd
+++ SpawnEnemy/Area2D.gd
@@ -1,10 +1,11 @@
-extends Area2D
+extends Node2D
+
 @onready var enemy_rute = get_node("/root/world_1/TileMap/Enemies")
 @onready var Enemies = [
-load("res://Enemies/enemy_1.tscn"),
-load("res://Enemies/enemy_2.tscn"),
-load("res://Enemies/monster_1.tscn"),
-load("res://Enemies/monster_2.tscn")	
+	load("res://Enemies/enemy_1.tscn"),
+	load("res://Enemies/enemy_2.tscn"),
+	load("res://Enemies/monster_1.tscn"),
+	load("res://Enemies/monster_2.tscn")	
 ]
 var bool_spawn = true
 var random = RandomNumberGenerator.new()
@@ -22,9 +23,10 @@ func spawn():
 		$Timer.start()
 		var i = random.randi_range(0,Enemies.size()-1)
 		bool_spawn = false
+		$Path2D/PathFollow2D.progress_ratio = random.randf()
 		var enemy_instance = Enemies[i].instantiate()
-		enemy_instance.position = Vector2(random.randi_range(80,45),randi_range(80,45))
-		enemy_rute.add_child(enemy_instance)
+		enemy_instance.position = $Path2D/PathFollow2D.global_position
+		add_child(enemy_instance)
 
 func _on_timer_timeout():
 	bool_spawn = true
