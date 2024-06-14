e481ee5c144bcc23a06420cef6c1179e2c350c37
Nicolás Lobos <nicolas.lobos01@alumnos.ucn.cl>
2024-06-14 18:13:45 -0400

    Barra de vida enemigos

---
+++ BarraVida/BarraVida.gd
@@ -0,0 +1,30 @@
+extends ProgressBar
+
+@onready var timer = $Timer
+@onready var BarraDaño = $"BarraDaño"
+
+var health = 0 : set = _set_health
+
+func _set_health(new_health):
+	var prev_health = health
+	health = min(max_value, new_health)
+	value = health
+	
+	if health<= 0:
+		queue_free()
+	
+	if health < prev_health:
+		timer.start()
+	else:
+		BarraDaño.value = health
+	
+func init_health(_healt):
+	health = _healt
+	max_value = health
+	value = health
+	BarraDaño.max_value = health
+	BarraDaño.value = health
+
+
+func _on_timer_timeout():
+	BarraDaño.value = health
