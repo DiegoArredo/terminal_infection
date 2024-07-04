93daa30710921c5ce36f6163d183b495c92846e7
brayanpizarro <brayan.pizarro@alumnos.ucn.cl>
2024-07-04 01:31:09 -0400

    crea gd state y finite state + carpeta boss1_state

---
+++ Enemies/Boss1_state/state.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+class_name  State
+
+@onready var player=owner.get_parent().find_child("Player_1")
+@onready var animacionPlayer=owner.find_child("AnimacionPlayer")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_physics_process(false)
+	
+func enter():
+	set_physics_process(true)
+	
+func exit():
+	set_physics_process(false)
+	
+func transition():
+	pass
+	
+func _physics_process(delta):
+	transition()
